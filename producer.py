2138e1968a6cde1ccce9489b3d9d1c7c8a17af10
Ezequiel Parziale <eparziale@gmail.com>
2021-10-16 17:44:15 -0300

    Producer funcionando

---
+++ producer.py
@@ -0,0 +1,24 @@
+from tweepy import Stream
+from kafka import KafkaProducer
+import config
+
+producer = KafkaProducer(bootstrap_servers=config.SERVER_KAFKA)
+
+class StdOutListener(Stream):
+  def on_data(self, data):
+    producer.send(config.TOPIC_NAME, data)
+    print(data)
+    return True
+
+  def on_error(self, status):
+    print(status)
+  
+if __name__ == '__main__':
+  stream  = StdOutListener(config.API_KEY, config.API_SECRET_KEY, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+
+  # Setting para la busqueda
+  tracks = config.TRACKS        # Palabras, usuarios, hastags a buscar
+  location = config.LOCATION    # Ubicacion area de tuits
+  languages = config.LANGUAGES  # Idioma de los tuis
+
+  stream.filter(track=tracks, locations=location, languages=languages)
