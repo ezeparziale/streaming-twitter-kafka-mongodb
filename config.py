8fb2cc0fb7b76ee5d99342ff1b553829ca4322f3
Ezequiel Parziale <eparziale@gmail.com>
2022-11-21 19:57:53 -0300

    :rocket: update: new config file with pydantic

--- config.py
+++ config.py
@@ -1,29 +1,30 @@
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
+from pydantic import BaseSettings
 
-dotenv_path = join(dirname(__file__), 'mongodb.env')
-load_dotenv(dotenv_path)
 
-dotenv_path = join(dirname(__file__), 'twitter_api.env')
-load_dotenv(dotenv_path)
+class Settings(BaseSettings):
 
-# API KEYS DE TWIITER
-API_KEY = os.environ.get("TWITTER_API_KEY")
-API_SECRET_KEY = os.environ.get("TWITTER_API_SECRET_KEY")
-ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+    # API KEYS DE TWIITER
+    TWITTER_API_KEY: str
+    TWITTER_API_SECRET_KEY: str
+    TWITTER_ACCESS_TOKEN: str
+    TWITTER_ACCESS_TOKEN_SECRET: str
 
-# MONGODB
-MONGODB_SERVER = 'localhost'
-MONGODB_USER = os.environ.get("MONGO_INITDB_ROOT_USERNAME")
-MONGODB_USER_PASSWORD = os.environ.get("MONGO_INITDB_ROOT_PASSWORD")
+    # MONGODB
+    MONGODB_SERVER: str = 'localhost'
+    MONGO_USERNAME: str
+    MONGO_PASSWORD: str
 
-# KAFKA
-SERVER_KAFKA = 'localhost:9092'
-TOPIC_NAME = 'twitter' # NOMBRE DEL TOPIC DE KAFKA
+    # KAFKA
+    SERVER_KAFKA: str = 'localhost:9092'
+    TOPIC_NAME: str = 'twitter' # NOMBRE DEL TOPIC DE KAFKA
 
-# CONFIGURACION PARA BUSQUEDA DE TUITS
-TRACKS = ['#argentina','argentina','seleccion','messi','escaloneta','afa']
-LOCATION = [-126.2,-56.0,22.3,58.9]
-LANGUAGES = ['en','es']
+    # CONFIGURACION PARA BUSQUEDA DE TUITS
+    TRACKS: list = ['#argentina','argentina','seleccion','messi','escaloneta','afa']
+    LOCATION: list = [-126.2,-56.0,22.3,58.9]
+    LANGUAGES: list = ['en','es']
+
+    class Config:
+        env_file = [".env"]
+
+
+settings = Settings()
